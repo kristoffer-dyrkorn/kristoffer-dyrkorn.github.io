f96a2b0e8359abc3d4f196cd3c2226fa2d29a8c2
Kristoffer Dyrkorn <kristoffer.dyrkorn@nrk.no>
2018-11-30 22:11:33 +0100

    First.

--- ar/js/index.js
+++ ar/js/index.js
@@ -49,7 +49,7 @@ scene.add(cube)
 
 const tileURL = "320750-7190500.png"
 
-const tileGeometry = new THREE.PlaneGeometry(TILE_EXTENTS, TILE_EXTENTS, 128, 128)
+const tileGeometry = new THREE.PlaneGeometry(100, 100, 128, 128)
 const tileMaterial = new THREE.MeshPhongMaterial()
 
 console.log("Loading tile: " + tileURL)
