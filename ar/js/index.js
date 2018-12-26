9109ecbd0a6ef9373e770d2989770c08d9dc0f93
Kristoffer Dyrkorn <kristoffer.dyrkorn@nrk.no>
2018-12-26 23:58:04 +0100

    First.

--- ar/js/index.js
+++ ar/js/index.js
@@ -107,7 +107,7 @@ function resetViewport() {
   }
 
   // resize plane according to camera y fov and aspect
-  plane.scale.y = Math.tan(camera.fov * 0.9 * THREE.Math.DEG2RAD) * PLANE_DISTANCE
+  plane.scale.y = Math.tan(camera.fov * 0.85 * THREE.Math.DEG2RAD) * PLANE_DISTANCE
   plane.scale.x = plane.scale.y * camera.aspect
 
   camera.updateProjectionMatrix()
