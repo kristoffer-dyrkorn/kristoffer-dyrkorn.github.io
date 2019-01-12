68640a19ff65c40daaae4f2a26d976f5f018f406
Kristoffer Dyrkorn <kristoffer.dyrkorn@nrk.no>
2019-01-12 23:07:24 +0100

    First.

--- ar/js/index.js
+++ ar/js/index.js
@@ -56,7 +56,7 @@ const planeGeometry = new THREE.CylinderBufferGeometry(
   1, // segments y
   true, // isOpen
   0, // startangle
-  360 * THREE.Math.DEG2RAD // angular length
+  180 * THREE.Math.DEG2RAD // angular length
 )
 //const planeMaterial = new THREE.MeshBasicMaterial({ map: videoTexture })
 const planeMaterial = new THREE.MeshBasicMaterial()
