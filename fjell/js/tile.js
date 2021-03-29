72273522026f990266371a4658ece4ec813b1368
Kristoffer Dyrkorn <kristoffer.dyrkorn@nrk.no>
2021-03-29 10:28:52 +0200

    First version

--- fjell/js/tile.js
+++ fjell/js/tile.js
@@ -1,77 +1,73 @@
-import * as THREE from "./three.module.js";
-import Logger from "./logger.js";
-import { OBJLoader } from "./OBJLoader.js";
+import * as THREE from "./three.module.js"
+import Logger from "./logger.js"
+import { OBJLoader } from "./OBJLoader.js"
 
-const tileServer = "/meshes";
-const poiServer = "/poi";
+const tileServer = "./fjell/meshes"
+const poiServer = "./fjell/poi"
 
 export default class Tile {
   constructor(x, y, camera, scene, font) {
-    this.x = x;
-    this.y = y;
+    this.x = x
+    this.y = y
 
-    this.camera = camera;
-    this.scene = scene;
-    this.font = font;
+    this.camera = camera
+    this.scene = scene
+    this.font = font
 
-    this.poiData = [];
-    this.poiCollection = new THREE.Group();
+    this.poiData = []
+    this.poiCollection = new THREE.Group()
   }
 
   load() {
-    const tileFile = `${this.x}-${this.y}.obj`;
-    const tileURL = `${tileServer}/${tileFile}`;
+    const tileFile = `${this.x}-${this.y}.obj`
+    const tileURL = `${tileServer}/${tileFile}`
 
-    console.log("Loading " + tileFile);
+    console.log("Loading " + tileFile)
 
     Tile.objLoader.load(
       tileURL,
       (object) => {
-        this.tileMesh = object.children[0];
+        this.tileMesh = object.children[0]
 
-        const wireframeGeometry = new THREE.WireframeGeometry(
-          this.tileMesh.geometry
-        );
-        const wireframeLines = new THREE.LineSegments(wireframeGeometry);
-        wireframeLines.position.set(this.x, this.y, 0);
+        const wireframeGeometry = new THREE.WireframeGeometry(this.tileMesh.geometry)
+        const wireframeLines = new THREE.LineSegments(wireframeGeometry)
+        wireframeLines.position.set(this.x, this.y, 0)
 
-        this.scene.add(wireframeLines);
-        this.loadPois();
+        this.scene.add(wireframeLines)
+        this.loadPois()
       },
       (xhr) => {},
       (error) => {
-        console.log("Could not load tile " + tileURL + ", " + error);
+        console.log("Could not load tile " + tileURL + ", " + error)
       }
-    );
+    )
   }
 
   // read all pois for the tile
   // and store poi xy location info in the this.poiData array
   async loadPois() {
-    const poiFile = `${this.x}-${this.y}.json`;
-    const poiURL = `${poiServer}/${poiFile}`;
+    const poiFile = `${this.x}-${this.y}.json`
+    const poiURL = `${poiServer}/${poiFile}`
 
-    console.log("Loading " + poiFile);
+    console.log("Loading " + poiFile)
 
-    const features = await fetch(poiURL).then((response) => response.json());
+    const features = await fetch(poiURL).then((response) => response.json())
     features.forEach((feature) => {
-      const position = feature.position;
-      position.push(0);
+      const position = feature.position
+      position.push(0)
 
       // note: here, the absolute location is stored
       this.poiData.push({
         type: feature.type,
         name: feature.name,
         position: position,
-      });
-    });
+      })
+    })
 
-    console.log(
-      "Done loading " + poiFile + ", " + this.poiData.length + " features."
-    );
+    console.log("Done loading " + poiFile + ", " + this.poiData.length + " features.")
 
     // show pois using default values
-    this.showPois(0, 36000, 0, 3000, "turisthytte");
+    this.showPois(0, 36000, 0, 3000, "turisthytte")
   }
 
   // using filters, create pois, add them to this.poiCollection and to the scene
@@ -83,78 +79,69 @@ export default class Tile {
       // scan all pois for this tile, applying specified filters
       // first, use the most easily calculated criterion
       if (feature.type === poiType) {
-        const position = feature.position;
+        const position = feature.position
 
         // then calculate poi distance to camera and use as criterion
         const distance =
-          (position[0] - this.camera.position.x) *
-            (position[0] - this.camera.position.x) +
-          (position[1] - this.camera.position.y) *
-            (position[1] - this.camera.position.y);
+          (position[0] - this.camera.position.x) * (position[0] - this.camera.position.x) +
+          (position[1] - this.camera.position.y) * (position[1] - this.camera.position.y)
 
         if (distance > minDist * minDist && distance < maxDist * maxDist) {
           // now, perform the costly elevation calculation
 
           // mesh triangles use coordinates relative to tile corner
           // so also use that here for ray casting
-          const poiPosition = new THREE.Vector3(
-            position[0] - this.x,
-            position[1] - this.y,
-            10000
-          );
+          const poiPosition = new THREE.Vector3(position[0] - this.x, position[1] - this.y, 10000)
 
           // find poi elevation by raycasting from above at poi xy location and down against mesh
-          const raycaster = new THREE.Raycaster();
-          raycaster.set(poiPosition, new THREE.Vector3(0, 0, -1));
-          const intersection = raycaster.intersectObject(this.tileMesh);
-          position[2] = intersection[0].point.z;
+          const raycaster = new THREE.Raycaster()
+          raycaster.set(poiPosition, new THREE.Vector3(0, 0, -1))
+          const intersection = raycaster.intersectObject(this.tileMesh)
+          position[2] = intersection[0].point.z
 
           // if elevation is inside bounds, create and add poi
           if (position[2] > minElev && position[2] < maxElev) {
-            const poi = this.buildPoi(feature.name);
+            const poi = this.buildPoi(feature.name)
             // note: here we use absolute coordinates
-            poi.position.set(position[0], position[1], position[2]);
+            poi.position.set(position[0], position[1], position[2])
 
             // rotate the poi towards the camera
             poi.rotateZ(
               270 * THREE.Math.DEG2RAD +
-                Math.atan2(
-                  poi.position.y - this.camera.position.y,
-                  poi.position.x - this.camera.position.x
-                )
-            );
+                Math.atan2(poi.position.y - this.camera.position.y, poi.position.x - this.camera.position.x)
+            )
 
-            this.poiCollection.add(poi);
+            this.poiCollection.add(poi)
           }
         }
       }
-    });
+    })
 
-    this.scene.add(this.poiCollection);
+    this.scene.add(this.poiCollection)
   }
 
   buildPoi(name) {
-    const poi = new THREE.Group();
+    const poi = new THREE.Group()
 
     // poi base (a sphere at the poi position)
-    const baseGeometry = new THREE.SphereGeometry(30, 8, 8);
+    const baseGeometry = new THREE.SphereGeometry(30, 8, 8)
     const baseMaterial = new THREE.MeshBasicMaterial({
       color: 0xff0000,
-    });
-    const base = new THREE.Mesh(baseGeometry, baseMaterial);
+    })
+    const base = new THREE.Mesh(baseGeometry, baseMaterial)
 
-    poi.add(base);
+    poi.add(base)
 
     // "sign pole", 300 m tall
-    const poleLength = 300;
-    const poleGeometry = new THREE.BoxGeometry(10, 10, poleLength);
-    const poleMaterial = new THREE.MeshBasicMaterial();
-    const pole = new THREE.Mesh(poleGeometry, poleMaterial);
+    const poleLength = 300
+    const poleGeometry = new THREE.BoxGeometry(10, 10, poleLength)
+    const poleMaterial = new THREE.MeshBasicMaterial()
+    const pole = new THREE.Mesh(poleGeometry, poleMaterial)
 
     // make pole end at the base
-    pole.position.set(0, 0, poleLength / 2);
+    pole.position.set(0, 0, poleLength / 2)
 
-    poi.add(pole);
+    poi.add(pole)
 
     // use distance to poi to make far signs larger (even out text sizes)
     //    const cameraDistance = this.camera.position.clone();
@@ -166,23 +153,23 @@ export default class Tile {
       font: this.font,
       size: 150,
       height: 1,
-    });
-    textGeometry.computeBoundingSphere();
+    })
+    textGeometry.computeBoundingSphere()
 
     const textMaterial = new THREE.MeshBasicMaterial({
       color: 0x00ff00,
-    });
-    const text = new THREE.Mesh(textGeometry, textMaterial);
+    })
+    const text = new THREE.Mesh(textGeometry, textMaterial)
 
     // make text orthogonal to the xy plane
-    text.rotateX(90 * THREE.Math.DEG2RAD);
+    text.rotateX(90 * THREE.Math.DEG2RAD)
 
     // center text and place on top of pole
-    text.position.set(-textGeometry.boundingSphere.radius, 0, poleLength);
+    text.position.set(-textGeometry.boundingSphere.radius, 0, poleLength)
 
-    poi.add(text);
-    return poi;
+    poi.add(text)
+    return poi
   }
 }
 
-Tile.objLoader = new OBJLoader();
+Tile.objLoader = new OBJLoader()
